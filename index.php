9d01d425a708f53641c5b91c547dc354e40fa07f
Kutyakin Micha <kutyakinma@modeltech.ru>
2018-12-23 00:28:53 +0300

    Форму авторизации. 1. В созданном на первом модуле файле index.php в корне сайта необходимо заставить работать форму авторизации. 2. Разместить в файле блок php с двумя переменными $login и $password. Присвоить им значения логина и пароля (придумать самостоятельно). 3. Форма авторизации должна выводиться на странице только при передаче GET параметра “login” в адресной строке браузера в значении “yes”. Вспоминаем про суперглобальный массив $_GET 4. Заполненная пользователем форма должна отправляться методом POST на эту же страницу. Для этого необходимо в атрибуте “action” тега “form” указать относительную ссылку на страницу index.php с необходимыми параметрами. 5. В случае отправки запроса методом POST (проверяем условием if суперглобальный массив $_POST на пустоту) на сервере должна осуществляться проверка логина и пароля введенных пользователем с заданными по умолчанию в переменных $login и $password соответственно. Необходимо проверить, что совпадает логин и совпадает пароль одновременно. Смотрим в суперглобальный массив $_POST. 6. При совпадении и логина, и пароля необходимо на странице вывести сообщение об успешной авторизации пользователя, которое необходимо подключить из файла /include/success.php. Вспоминаем про функции подключения файлов. В случае несовпадения либо логина, либо пароля необходимо вывести форму авторизации с сообщением: “Неверный логин или пароль”. При этом в поля формы необходимо подставить данные, которые ввел пользователь ранее. Для этого в атрибуты “value” тегов “input” необходимо подставить данные, которые пришли из формы или подставлять пустые строки, если данных нет. Вспоминаем про замечательный тернарный оператор и используем его. 7. Необходимо создать отдельный файл с массивом логинов пользователей и отдельный файл с массивом паролей пользователей. Заполнить оба файла придуманными значениями. Подключить оба файла на странице авторизации. Осуществлять проверку из пункта 5 с логинами и паролями из файлов. При этом к каждому из логинов (описанных в файле) должен подходить только один пароль (из описанных в файле)

--- index.php
+++ index.php
@@ -1,4 +1,49 @@
 <?php
+$login = $password = '';
+$successAuth = $failureAuth = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $loginErr = $passwordErr = '';
+    if (empty($_POST['login'])) {
+        $loginErr = 'Необходим логин';
+    } else {
+        $login = test_input($_POST['login']);
+    }
+    if (empty($_POST['password'])) {
+        $passwordErr = 'Необходим пароль';
+    } else {
+        $password = test_input($_POST['password']);
+    }
+
+    if (!$loginErr && !$passwordErr) {
+        if (checkAuth($login, $password)) {
+            $successAuth = require_once $_SERVER['DOCUMENT_ROOT'] . '/include/successAuth.php';
+            $login = $password = '';
+        } else {
+            $failureAuth = require_once $_SERVER['DOCUMENT_ROOT'] . '/include/failureAuth.php';
+        }
+    }
+}
+
+function checkAuth($login, $password)
+{
+    $loginStore = require_once $_SERVER['DOCUMENT_ROOT'] . '/auth/loginStore.php';
+    if (in_array($login, $loginStore)) {
+        $passwordStore = require_once $_SERVER['DOCUMENT_ROOT'] . '/auth/passwordStore.php';
+        if (isset($passwordStore[$login]) && $password === $passwordStore[$login]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
@@ -24,33 +69,54 @@
             <p>Вести свои личные списки, например покупки в магазине, цели, задачи и многое другое. Делится списками с
                 друзьями и просматривать списки друзей.</p>
 
-
+            <a href="/?login=true">Перейти к авторизации</a>
         </td>
-        <td class="right-column-index">
-
-            <div class="project-folders-menu">
-                <ul class="project-folders-v">
-                    <li class="project-folders-v-active"><span>Авторизация</span></li>
-                    <li><a href="#">Регистрация</a></li>
-                    <li><a href="#">Забыли пароль?</a></li>
-                </ul>
-                <div style="clear: both;"></div>
-            </div>
-            <div class="index-auth">
-                <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                    <tr>
-                        <td class="iat">Ваш e-mail: <br/> <input id="login_id" size="30" name="login"/></td>
-                    </tr>
-                    <tr>
-                        <td class="iat">Ваш пароль: <br/> <input id="login_id" size="30" name="login"/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="button" value="Войти"/></td>
-                    </tr>
-                </table>
-            </div>
-
-        </td>
+        <?php if (filter_var($_GET['login'], FILTER_VALIDATE_BOOLEAN)): ?>
+            <td class="right-column-index">
+                <div class="project-folders-menu">
+                    <ul class="project-folders-v">
+                        <li class="project-folders-v-active"><span>Авторизация</span></li>
+                        <li><a href="#">Регистрация</a></li>
+                        <li><a href="#">Забыли пароль?</a></li>
+                    </ul>
+                    <div style="clear: both;"></div>
+                </div>
+                <div class="index-auth">
+                    <form action="/?login=true"
+                          method="post"
+                          width="100%"
+                          border="0"
+                          cellspacing="0"
+                          cellpadding="0">
+                        Ваш логин: <input type="text"
+                                           id="auth_login"
+                                           name="login"
+                                           size="30"
+                                           value="<?= $login; ?>">
+                        <?php if ($loginErr): ?>
+                            <span class="error"> <?= $loginErr ?></span>
+                        <?php endif; ?>
+                        <br><br>
+                        Ваш пароль: <input type="password"
+                                           id="auth_password"
+                                           name="password"
+                                           size="30"
+                                           value="<?= $password; ?>">
+                        <?php if ($passwordErr): ?>
+                            <span class="error"> <?= $passwordErr ?></span>
+                        <?php endif; ?>
+                        <br><br>
+                        <input type="submit" name="submit" value="Войти">
+                        <?php if ($successAuth): ?>
+                            <p class="success"><?= $successAuth ?></p>
+                        <?php endif; ?>
+                        <?php if ($failureAuth): ?>
+                            <p class="error"><?= $failureAuth ?></p>
+                        <?php endif; ?>
+                    </form>
+                </div>
+            </td>
+        <?php endif; ?>
     </tr>
 </table>
 
