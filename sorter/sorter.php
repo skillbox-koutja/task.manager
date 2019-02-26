35b37e4bd4a8aba912af18424a4b05edb79b0e3c
Kutyakin Micha <kutyakinma@modeltech.ru>
2019-02-26 23:41:22 +0300

    зарефакторил код убрал лишний код

--- sorter/sorter.php
+++ sorter/sorter.php
@@ -1,38 +1,27 @@
 <?php
 
-function createSorterComparison($direction = SORT_ASC, $property = null)
+function createSorterComparison($k = 'sort', $sort = SORT_ASC)
 {
-    switch ($direction) {
-        case SORT_ASC:
-            $sorter = function ($a, $b) {
-                return $a <=> $b;
-            };
-            break;
-        case SORT_DESC:
-            $sorter = function ($a, $b) {
-                return $b <=> $a;
-            };
-            break;
-        default:
-            throw new \InvalidArgumentException(
-                sprintf(
-                    'Invalid sort direction "%s". Allowed list: %s',
-                    $direction,
-                    '"' . implode('", "', ['SORT_ASC', 'SORT_DESC']) .'"'
-                )
-            );
-    }
-
-    if ($property) {
-        $sorter = function ($a, $b) use ($sorter, $property) {
-            return $sorter($a[$property], $b[$property]);
+    if ($sort === SORT_ASC) {
+        return function ($a, $b) use ($k) {
+            return $a[$k] <=> $b[$k];
         };
+    } else if ($sort === SORT_DESC) {
+        return function ($b, $a) use ($k) {
+            return $a[$k] <=> $b[$k];
+        };
+    } else {
+        throw new \InvalidArgumentException(
+            sprintf(
+                'Invalid sort direction. Allowed list: %s',
+                '"' . implode('", "', ['SORT_ASC', 'SORT_DESC']) . '"'
+            )
+        );
     }
-    return $sorter;
 }
 
 function arraySort(array $array, $key = 'sort', $sort = SORT_ASC)
 {
-    usort($array, createSorterComparison($sort, $key));
+    usort($array, createSorterComparison( $key, $sort));
     return $array;
 }
