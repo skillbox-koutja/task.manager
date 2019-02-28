7fa40b734c0cf8ec7fc0316531d3eb49fa79b217
Kutyakin Micha <kutyakinma@modeltech.ru>
2019-02-28 12:02:54 +0300

    Функцию createSorterComparison cделал в "одну строку"

--- sorter/sorter.php
+++ sorter/sorter.php
@@ -2,22 +2,11 @@
 
 function createSorterComparison($k = 'sort', $sort = SORT_ASC)
 {
-    if ($sort === SORT_ASC) {
-        return function ($a, $b) use ($k) {
-            return $a[$k] <=> $b[$k];
-        };
-    } else if ($sort === SORT_DESC) {
-        return function ($b, $a) use ($k) {
-            return $a[$k] <=> $b[$k];
-        };
-    } else {
-        throw new \InvalidArgumentException(
-            sprintf(
-                'Invalid sort direction. Allowed list: %s',
-                '"' . implode('", "', ['SORT_ASC', 'SORT_DESC']) . '"'
-            )
-        );
-    }
+    return $sort === SORT_DESC ? function ($a, $b) use ($k) {
+        return $a[$k] <=> $b[$k];
+    } : function ($b, $a) use ($k) {
+        return $a[$k] <=> $b[$k];
+    };
 }
 
 function arraySort(array $array, $key = 'sort', $sort = SORT_ASC)
