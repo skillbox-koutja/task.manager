178ca3905d1da2afd0bcae3c832ef81520a53c6a
Кутякин Михаил <michakoutjakin@gmail.com>
2019-03-02 14:15:26 +0300

    Задание со звездочкой: 1. Необходимо сделать так, чтобы идентификатор сессии был не PHPSESSID, а session_id 2. Время жизни сессии и сессионной куки должно быть установлено 20 минут.

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/constants/session.php';
 
-
+session_name('session_id');
 session_start();
+setcookie(session_name(), session_id(), time() + LIFETIME_SESSION);
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/auth/auth.php';
 
