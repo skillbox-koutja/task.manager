165c0008fe7062dbd87169f205fad5d795adf723
Кутякин Михаил <michakoutjakin@gmail.com>
2019-03-02 11:33:59 +0300

    Объедините функции arraySort и createSorterComparison

--- sorter/sorter.php
+++ sorter/sorter.php
@@ -1,16 +1,9 @@
 <?php
 
-function createSorterComparison($k = 'sort', $sort = SORT_ASC)
-{
-    return $sort === SORT_DESC ? function ($a, $b) use ($k) {
-        return $a[$k] <=> $b[$k];
-    } : function ($b, $a) use ($k) {
-        return $a[$k] <=> $b[$k];
-    };
-}
-
 function arraySort(array $array, $key = 'sort', $sort = SORT_ASC)
 {
-    usort($array, createSorterComparison( $key, $sort));
+    usort($array, function ($a, $b) use ($key, $sort) {
+        return $sort === SORT_DESC ? $b[$key] <=> $a[$key] : $a[$key] <=> $b[$key];
+    });
     return $array;
 }
