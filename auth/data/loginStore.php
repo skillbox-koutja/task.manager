4c4b54878ddfa6d9506e75a32beb191ba5015c8b
Kutyakin Micha <kutyakinma@modeltech.ru>
2019-03-04 21:31:33 +0300

    1. В учебных целях включил файлы с паролями в git.

---
+++ auth/data/loginStore.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'login1',
+    'login2',
+    'login3',
+    'login4',
+    'login5'
+];
---
+++ auth/data/passwordStore.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    '123456',
+    'qwerty',
+    'qwe123',
+    'password',
+    '111111'
+];
