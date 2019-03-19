a3f4ed8c2dbe0bbc839e01eae0879131417fe7f8
Kutyakin Micha <kutyakinma@modeltech.ru>
2019-03-19 22:42:29 +0300

    Поправил имена исходя замечаний. Убрал связь многие ко многим для писем и разделов сообщений

--- migrations/20190316000100-create_db.sql
+++ migrations/20190316000100-create_db.sql
@@ -1,4 +1,6 @@
 # https://docs.google.com/document/d/1TfqAVC-R2KMvIp4L71bMyUy9IKA14NcE0JuKAR86ha4/edit
+# drop database unotify;
+
 
 create database if not exists unotify CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci charset utf8mb4;
 
@@ -8,7 +10,7 @@ create table app_user
 (
   id int unsigned auto_increment not null comment 'идентификатор пользователя',
   email varchar(255) not null,
-  password varchar(32) not null,
+  pw varchar(32) not null,
   is_enabled bool default 1 null comment 'флаг активности пользователя (активен/неактивен)',
   recv_email bool default 1 null comment 'флаг согласия на получение уведомлений по email (согласен / не согласен)',
   phone varchar(15) null,
@@ -19,9 +21,9 @@ create table app_user
     primary key (id)
 );
 
-create unique index app_user_email_uinx
+create unique index user_email_uinx
   on app_user (email);
-create unique index app_user_phone_uinx
+create unique index user_phone_uinx
   on app_user (phone);
 
 create table app_group
@@ -33,31 +35,63 @@ create table app_group
     primary key (id)
 );
 
-create unique index app_group_caption_uinx
+create unique index group_caption_uinx
   on app_group (caption);
 
-create table app_user_groups
+create table group_user
 (
   user_id int unsigned not null,
   group_id int unsigned not null,
-  constraint app_user_groups_pk
+  constraint group_user_pk
     primary key (user_id, group_id),
-  constraint app_user_groups_group_fk
+  constraint group_user_g_fk
     foreign key (group_id) references app_group (id)
       on update cascade on delete cascade,
-  constraint app_user_groups_user_fk
+  constraint group_user_u_fk
     foreign key (user_id) references app_user (id)
       on update cascade on delete cascade
+);
+
+create table color
+(
+  id int unsigned not null auto_increment comment 'id цвет',
+  hex_value varchar(6) not null comment 'шестнадцатеричное значение',
+  constraint color_pk
+    primary key (id)
+);
+create unique index color_hex_uinx
+  on color (hex_value);
+
+create table section
+(
+  id int unsigned not null auto_increment comment 'идентификатор раздела сообщения',
+  caption nvarchar(255) comment 'заголовок',
+  created_at datetime not null comment 'дата создания раздела',
+  created_by int unsigned comment 'создатель раздела',
+  color_id int unsigned null comment 'цвет',
+  parent_id int unsigned null comment 'родительский раздел',
+  constraint section_pk
+    primary key (id),
+  constraint section_user_fk
+    foreign key (created_by) references app_user (id)
+      on update cascade on delete cascade,
+  constraint section_parent_fk
+    foreign key (parent_id) references section (id)
+      on update cascade on delete cascade,
+  constraint section_color_fk
+    foreign key (color_id) references color (id)
+      on update cascade on delete set null
 );
 
 create table msg
 (
   id bigint unsigned not null auto_increment comment 'идентификатор сообщения',
-  subject nvarchar(255) comment 'заголовок',
+  title nvarchar(255) comment 'заголовок',
   body nvarchar(2000) not null comment 'текст сообщения',
   created_at datetime not null comment 'дата отправки',
   from_id int unsigned comment 'отправитель',
   to_id int unsigned comment 'получатель',
+  section_id int unsigned comment 'получатель',
   is_read bool default 0 comment 'флаг, что сообщение прочитано',
   constraint message_pk
     primary key (id),
@@ -66,50 +100,8 @@ create table msg
       on update cascade on delete cascade,
   constraint msg_to_fk
     foreign key (to_id) references app_user (id)
-      on update cascade on delete cascade
-);
-
-create table msg_section_color
-(
-  id int unsigned not null auto_increment comment 'id цвет',
-  hex_value varchar(6) not null comment 'шестнадцатеричное значение',
-  constraint msg_section_color_pk
-    primary key (id)
-);
-create unique index msg_section_color_hex_uinx
-  on msg_section_color (hex_value);
-
-create table msg_section
-(
-  id bigint unsigned not null auto_increment comment 'идентификатор раздела сообщения',
-  caption nvarchar(255) comment 'заголовок',
-  created_at datetime not null comment 'дата создания раздела',
-  created_by int unsigned comment 'создатель раздела',
-  color_id int unsigned null comment 'цвет',
-  parent_id bigint unsigned null comment 'родительский раздел',
-  constraint msg_section_pk
-    primary key (id),
-  constraint msg_section_user_fk
-    foreign key (created_by) references app_user (id)
       on update cascade on delete cascade,
-  constraint msg_section_parent_fk
-    foreign key (parent_id) references msg_section (id)
-      on update cascade on delete cascade,
-  constraint msg_section_color_fk
-    foreign key (color_id) references msg_section_color (id)
-      on update cascade on delete set null
-);
-
-create table msg_section_assoc
-(
-  msg_id bigint unsigned not null,
-  section_id bigint unsigned not null,
-  constraint msg_section_assoc_pk
-    primary key (msg_id, section_id),
-  constraint msg_section_assoc_msg_fk
-    foreign key (msg_id) references msg (id)
-      on update cascade on delete cascade,
-  constraint msg_section_assoc_section_fk
-    foreign key (section_id) references msg_section (id)
+  constraint msg_section_fk
+    foreign key (section_id) references section (id)
       on update cascade on delete cascade
 );
--- migrations/20190317000100-insert-fixtures.sql
+++ migrations/20190317000100-insert-fixtures.sql
@@ -1,88 +1,88 @@
 use unotify;
 
-insert into app_user (email, password, is_enabled, recv_email, phone, first_name, last_name, middle_name)
+insert into app_user (email, pw, is_enabled, recv_email, phone, first_name, last_name, middle_name)
 values ('login1@mail.ru', '123456', 1, 1, '+79103331101', 'Иван', 'Иванов', 'Иванович'),
-('login2@mail.ru', 'qwerty', 1, 1, '+79103331102', 'Герасим', 'Сергеев', 'Джавскирпитович'),
-('login3@mail.ru', 'qwe123', 1, 1, '+79103331103', 'Митрофан', 'Витальев', 'Реквайрхедорович'),
-('login4@mail.ru', 'password', 1, 1, '+79103331104', 'Волк', 'Михайлов', 'Респонсович'),
-('login5@mail.ru', '111111', 1, 1, '+79103331105', 'Бессон', 'Ильин', 'Инлюдфутерович');
+       ('login2@mail.ru', 'qwerty', 1, 1, '+79103331102', 'Герасим', 'Сергеев', 'Джавскирпитович'),
+       ('login3@mail.ru', 'qwe123', 1, 1, '+79103331103', 'Митрофан', 'Витальев', 'Реквайрхедорович'),
+       ('login4@mail.ru', 'password', 1, 1, '+79103331104', 'Волк', 'Михайлов', 'Респонсович'),
+       ('login5@mail.ru', '111111', 1, 1, '+79103331105', 'Бессон', 'Ильин', 'Инлюдфутерович');
 
 insert into app_group (caption, description)
 VALUES ('registered', 'Зарегистрированный пользователь'),
-('access_write_message', 'Пользователь имеющий право писать сообщения');
+       ('access_write_message', 'Пользователь имеющий право писать сообщения');
 
-insert into app_user_groups (user_id, group_id)
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login1@mail.ru') user_id,
-(select id from app_group where caption = 'registered')  group_id;
-insert into app_user_groups (user_id, group_id)
+       (select id from app_group where caption = 'registered')  group_id;
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login2@mail.ru') user_id,
-(select id from app_group where caption = 'registered')  group_id;
-insert into app_user_groups (user_id, group_id)
+       (select id from app_group where caption = 'registered')  group_id;
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login3@mail.ru') user_id,
-(select id from app_group where caption = 'registered')  group_id;
-insert into app_user_groups (user_id, group_id)
+       (select id from app_group where caption = 'registered')  group_id;
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login4@mail.ru') user_id,
-(select id from app_group where caption = 'registered')  group_id;
-insert into app_user_groups (user_id, group_id)
+       (select id from app_group where caption = 'registered')  group_id;
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login5@mail.ru') user_id,
-(select id from app_group where caption = 'registered')  group_id;
-insert into app_user_groups (user_id, group_id)
+       (select id from app_group where caption = 'registered')  group_id;
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login1@mail.ru')          user_id,
-(select id from app_group where caption = 'access_write_message') group_id;
-insert into app_user_groups (user_id, group_id)
+       (select id from app_group where caption = 'access_write_message') group_id;
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login2@mail.ru')          user_id,
-(select id from app_group where caption = 'access_write_message') group_id;
-insert into app_user_groups (user_id, group_id)
+       (select id from app_group where caption = 'access_write_message') group_id;
+insert into group_user (user_id, group_id)
 select (select id from app_user where email = 'login3@mail.ru')          user_id,
-(select id from app_group where caption = 'access_write_message') group_id;
+       (select id from app_group where caption = 'access_write_message') group_id;
 
-insert into msg_section_color (hex_value)
+insert into color (hex_value)
 values ('00ffff') /*aqua*/,
-('808080') /*gray*/,
-('000080') /*navy*/,
-('c0c0c0') /*silver*/,
-('000000') /*black*/,
-('008000') /*green*/,
-('808000') /*olive*/,
-('008080') /*teal*/,
-('0000ff') /*blue*/,
-('00ff00') /*lime*/,
-('800080') /*purple*/,
-('ffffff') /*white*/,
-('ff00ff') /*fuchsia*/,
-('maroon') /*maroon*/,
-('ff0000') /*red*/,
-('ffff00') /*yellow*/;
+       ('808080') /*gray*/,
+       ('000080') /*navy*/,
+       ('c0c0c0') /*silver*/,
+       ('000000') /*black*/,
+       ('008000') /*green*/,
+       ('808000') /*olive*/,
+       ('008080') /*teal*/,
+       ('0000ff') /*blue*/,
+       ('00ff00') /*lime*/,
+       ('800080') /*purple*/,
+       ('ffffff') /*white*/,
+       ('ff00ff') /*fuchsia*/,
+       ('maroon') /*maroon*/,
+       ('ff0000') /*red*/,
+       ('ffff00') /*yellow*/;
 
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'Основные', '2019-03-17 00:14:30', (select id from app_user where email = 'login1@mail.ru'),
-(select id from msg_section_color where hex_value = '00ff00'), null;
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+       (select id from color where hex_value = '00ff00'), null;
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'по работе', '2019-03-17 00:14:31', (select id from app_user where email = 'login1@mail.ru'),
-(select id from msg_section_color where hex_value = '000080'),
-(select id from msg_section where caption = 'Основные');
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+       (select id from color where hex_value = '000080'),
+       (select id from section where caption = 'Основные');
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'личные', '2019-03-17 00:14:32', (select id from app_user where email = 'login1@mail.ru'),
-(select id from msg_section_color where hex_value = '00ffff'),
-(select id from msg_section where caption = 'Основные');
+       (select id from color where hex_value = '00ffff'),
+       (select id from section where caption = 'Основные');
 
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'Оповещения', '2019-03-17 00:15:30', (select id from app_user where email = 'login2@mail.ru'),
-(select id from msg_section_color where hex_value = 'ff0000'), null;
+       (select id from color where hex_value = 'ff0000'), null;
 
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'форумы', '2019-03-17 00:15:31', (select id from app_user where email = 'login2@mail.ru'),
-(select id from msg_section_color where hex_value = 'ff00ff'),
-(select id from msg_section where caption = 'Оповещения');
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+       (select id from color where hex_value = 'ff00ff'),
+       (select id from section where caption = 'Оповещения');
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'магазины', '2019-03-17 00:15:32', (select id from app_user where email = 'login2@mail.ru'),
-(select id from msg_section_color where hex_value = 'ffff00'),
-(select id from msg_section where caption = 'Оповещения');
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+       (select id from color where hex_value = 'ffff00'),
+       (select id from section where caption = 'Оповещения');
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'подписки', '2019-03-17 00:15:33', (select id from app_user where email = 'login2@mail.ru'),
-(select id from msg_section_color where hex_value = '800080'),
-(select id from msg_section where caption = 'Оповещения');
+       (select id from color where hex_value = '800080'),
+       (select id from section where caption = 'Оповещения');
 
-insert into msg_section (caption, created_at, created_by, color_id, parent_id)
+insert into section (caption, created_at, created_by, color_id, parent_id)
 select 'Спам', '2019-03-17 00:16:30', (select id from app_user where email = 'login3@mail.ru'),
-(select id from msg_section_color where hex_value = 'ff0000'), null;
+       (select id from color where hex_value = 'ff0000'), null;
